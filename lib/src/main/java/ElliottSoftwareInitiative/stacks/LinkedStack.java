f34ff2c687b2fbd8355e1011a06285819f5b8499
Tristan Elliott <>
2021-11-29 12:03:26 -0400

    adding the singlyLinkedList stack implementation

---
+++ lib/src/main/java/ElliottSoftwareInitiative/stacks/LinkedStack.java
@@ -0,0 +1,33 @@
+package ElliottSoftwareInitiative.stacks;
+
+import ElliottSoftwareInitiative.linkedLists.SinglyLinkedList;
+
+public class LinkedStack <E> implements Stack<E>{
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public E top() {
+        return list.first();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+}
