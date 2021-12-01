a1d02c74942557d9e1546d22c9b400fbc4482d0a
Tristan Elliott <>
2021-12-01 13:16:46 -0400

    adding linked list stack

--- lib/src/main/java/ElliottSoftwareInitiative/stacks/LinkedStack.java
+++ lib/src/main/java/ElliottSoftwareInitiative/stacks/LinkedStack.java
@@ -5,7 +5,7 @@ import ElliottSoftwareInitiative.linkedLists.SinglyLinkedList;
 /**
  * THE SINGLY LINKED LIST STACK IMPLEMENTATION
  * **/
-public class LinkedStack <E> implements Stack<E>{
+public class LinkedStack<E> implements Stack<E>{
     private SinglyLinkedList<E> list = new SinglyLinkedList<>();
 
     @Override
@@ -26,7 +26,6 @@ public class LinkedStack <E> implements Stack<E>{
     @Override
     public void push(E e) {
         list.addFirst(e);
-
     }
 
     @Override
@@ -34,3 +33,14 @@ public class LinkedStack <E> implements Stack<E>{
         return list.removeFirst();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
