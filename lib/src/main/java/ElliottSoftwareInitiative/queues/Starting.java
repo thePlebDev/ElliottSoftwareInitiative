45598042e9fe26f64d4e1649ed12db2c1fdfb7e5
Tristan Elliott <>
2021-12-05 14:12:27 -0400

    adding queue implementations

---
+++ lib/src/main/java/ElliottSoftwareInitiative/queues/Starting.java
@@ -0,0 +1,10 @@
+package ElliottSoftwareInitiative.queues;
+
+public class Starting {
+
+    public static void main(String[] args){
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>(5);
+
+
+    }
+}
